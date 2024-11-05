8ad93add1655c1f2cd114cc9264baeee25519450
shshdxk <609022045@qq.com>
2024-11-05 18:12:13 +0800

    修改名称
    修改名称

--- 3.5/3.5.3/build.sh
+++ 3.5/3.5.3/build.sh
@@ -1,3 +1,3 @@
 wget -O spark-connect_2.12-3.5.3.jar -c https://repo1.maven.org/maven2/org/apache/spark/spark-connect_2.12/3.5.3/spark-connect_2.12-3.5.3.jar
-docker build -t shshdxk/delta-lake-connect:3.5.3 .
+docker build -t shshdxk/delta-lake-spark-connect:3.5.3 .
 
